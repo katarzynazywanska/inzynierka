47d044f931c4eef6dc7e0969fd72e7196daae4bf
katarzynazywanska <kasia_zywanska@wp.pl>
2021-11-03 22:43:03 +0100

    layout with unsolved error

--- sample-app/src/main/java/com/kontakt/sample/Dashboard.java
+++ sample-app/src/main/java/com/kontakt/sample/Dashboard.java
@@ -3,11 +3,26 @@ package com.kontakt.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
 public class Dashboard extends AppCompatActivity {
+RecyclerView recyclerView;
+RecyclerAdapter adapter;
+
+String rooms[] = {"235, 236, 237, 238, 239, 240"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        getSupportActionBar().hide();
+
+        recyclerView = findViewById(R.id.recView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerAdapter(this,rooms);
+        recyclerView.setAdapter(adapter);
+
     }
 }
